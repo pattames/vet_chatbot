3d1aca922c58c3551c0321f7e1587d96800973bc
Patricio Tames <patriciotames@gmail.com>
2025-10-01 15:52:58 +0000

    fix: sentence transformer argument

--- vector_db.py
+++ vector_db.py
@@ -13,9 +13,7 @@ DB_PATH = "./vector_db"
 chroma_client = chromadb.PersistentClient(path=DB_PATH)
 
 # Use Sentence Transformers embedding function
-embedding_function = SentenceTransformer(
-   model_name="PlanTL-GOB-ES/bsc-bio-ehr-es" # For medical in spanish
-)
+embedding_function = SentenceTransformer("PlanTL-GOB-ES/bsc-bio-ehr-es") # For spanish and medical knowledge
 
 # Create or get collection in ChromaDB
 # The embedding function will be used when adding documents
@@ -367,7 +365,7 @@ def reset_collection(): # Use when modified knowledge base, changed embedding mo
 
 if __name__ == "__main__":
    # Optional: Reset collection for fresh start (also delete folder inside vector_db):
-#    reset_collection()
+   reset_collection()
 
    # Check collection
 #    print(collection.get())
@@ -386,8 +384,8 @@ if __name__ == "__main__":
       "Gato adulto con vómitos y mal aliento",
    ]
 
-   for query in test_queries:
-      print(f"\n{'='*60}")
-      print(f"QUERY: {query}")
-      response = query_diseases(query)
-      print(f"\nRESPONSE:\n{response}\n")
+#    for query in test_queries:
+#       print(f"\n{'='*60}")
+#       print(f"QUERY: {query}")
+#       response = query_diseases(query)
+#       print(f"\nRESPONSE:\n{response}\n")
